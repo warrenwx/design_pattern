ca2d64597d2ca2e08feea9e978c1e09800e493b8
warren <wangx@seu.edu.cn>
2018-10-18 18:05:32 +0800

    update

---
+++ Test/src/org/wx/test/nio/MyClient.java
@@ -0,0 +1,44 @@
+package org.wx.test.nio;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class MyClient {
+	private static final int BUFFER_SIZE = 1024;	
+	static InputStreamReader isr = new InputStreamReader(System.in);
+	
+	public static void main(String[] args) throws IOException {
+
+		logger("Starting MyClient...");
+		SocketChannel myClient = null;
+		try {
+			int port = 9999;
+			myClient = SocketChannel.open(new InetSocketAddress(InetAddress.getLocalHost(), port));
+
+			logger(String.format("Trying to connect to %s:%d...", InetAddress.getLocalHost(), port));
+			while(true) {
+				char[] cbuf = new char[1024];
+				isr.read(cbuf);
+				ByteBuffer myBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+				myBuffer.put(new String(cbuf).getBytes());
+				myBuffer.flip();
+				int bytesWritten = myClient.write(myBuffer);
+				logger(String.format("Sending Message...: %s bytesWritten...: %d\n", cbuf.toString(), bytesWritten));
+			}					
+		} catch (IOException e) {
+			logger(e.getMessage());
+			e.printStackTrace();
+		}finally {
+			logger("Closing Client connection...");
+			myClient.close();
+		}
+	}
+
+	public static void logger(String msg) {
+		System.out.println(msg);
+	}
+}
