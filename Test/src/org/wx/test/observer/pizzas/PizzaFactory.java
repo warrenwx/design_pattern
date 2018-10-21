3f03866078f86f4a3171c0cd59fa67499b819067
tomx <wangx@seu.edu.cn>
2018-10-21 21:37:35 +0800

    update

--- Test/src/org/wx/test/observer/pizzas/PizzaFactory.java
+++ Test/src/org/wx/test/observer/pizzas/PizzaFactory.java
@@ -1,7 +1,7 @@
 package org.wx.test.observer.pizzas;
 
 public class PizzaFactory {
-	public void createPizza(String pizzaType){
+	public Pizza createPizza(String pizzaType){
 		if (pizzaType == "newyorkstyle") {
 			System.out.println("newyork style pizza.");
 		}else if (pizzaType == "chicagostype") {
